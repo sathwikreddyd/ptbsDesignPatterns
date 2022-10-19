357deaa16f37ec03703b0d0d790854b8acf01e64
DSR <dollaking550@gmail.com>
2022-10-19 10:01:18 -0700

    product iterator

--- src/ProductIterator.java
+++ src/ProductIterator.java
@@ -2,20 +2,30 @@ public class ProductIterator {
 
 	private ClassProductList productList;
 
+	private int presentIterator;
+
+	public ProductIterator(ClassProductList productList) {
+		this.productList = productList;
+		this.presentIterator = 0;
+	}
+
 	public boolean hasNext() {
+		if(presentIterator<productList.size()){
+			return true;
+		}
 		return false;
 	}
 
 	public Product next() {
-		return null;
+		return ProductList.get(this.presentIterator);
 	}
 
 	public void moveToHead() {
-
+		this.presentIterator = 0;
 	}
 
 	public void remove() {
-
+		productList.remove(this.presentIterator);
 	}
 
 }
