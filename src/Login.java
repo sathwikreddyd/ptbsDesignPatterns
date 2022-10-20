450435991ba5dd2170f80717bb0d77d8a4c873a5
DSR <dollaking550@gmail.com>
2022-10-20 02:10:32 -0700

    changes to login

--- src/Login.java
+++ src/Login.java
@@ -3,49 +3,62 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class Login extends JFrame implements ActionListener {
-   JPanel panel;
-   JLabel user_label, password_label, message;
-   JTextField userName_text;
-   JPasswordField password_text;
-   JButton submit, cancel;
-   Login() {
-      // Username Label
-      user_label = new JLabel();
-      user_label.setText("User Name :");
-      userName_text = new JTextField();
-      // Password Label
+   private JPanel window;
+   private JLabel login_label,empty_label,userName_label, password_label, status_label;
+   private JTextField userName_text,password_text;
+   private JButton submit;
+   private int loginStatus;
+   public int LoginWindow() {
+      login_label = new JLabel();
+      empty_label = new JLabel();
+      userName_label = new JLabel();
       password_label = new JLabel();
-      password_label.setText("Password :");
-      password_text = new JPasswordField();
-      // Submit
-      submit = new JButton("SUBMIT");
-      panel = new JPanel(new GridLayout(3, 1));
-      panel.add(user_label);
-      panel.add(userName_text);
-      panel.add(password_label);
-      panel.add(password_text);
-      message = new JLabel();
-      panel.add(message);
-      panel.add(submit);
-      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      // Adding the listeners to components..
+      status_label = new JLabel();
+      login_label.setText("LOGIN");
+      empty_label.setText("Enter Details");
+      userName_label.setText("User Name");
+      password_label.setText("Password");
+      status_label.setText(" ");
+      userName_text = new JTextField();
+      password_text = new JTextField();
+      submit = new JButton("Submit");
+      window = new JPanel(new GridLayout(4, 2));
+      window.add(login_label);
+      window.add(empty_label);
+      window.add(userName_label);
+      window.add(userName_text);
+      window.add(password_label);
+      window.add(password_text);
+      window.add(submit);
+      window.add(status_label);
       submit.addActionListener(this);
-      add(panel, BorderLayout.CENTER);
-      setTitle("Please Login Here !");
-      setSize(450,350);
+      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+      add(window, BorderLayout.CENTER);
+      setTitle("Login");
+      setSize(500,500);
       setVisible(true);
+      return loginStatus;
+   }
+
+   public void sett(){
+      System.out.println(loginStatus);
    }
    public static void main(String[] args) {
-      new Login();
+      Login k = new Login();
+      System.out.println(k.LoginWindow());
+      k.sett();
    }
    @Override
    public void actionPerformed(ActionEvent ae) {
       String userName = userName_text.getText();
       String password = password_text.getText();
       if (userName.trim().equals("admin") && password.trim().equals("admin")) {
-         message.setText(" Hello " + userName + "");
+         status_label.setText(" Hello " + userName + "");
+         loginStatus = 15;
+
       } else {
-         message.setText(" Invalid user.. ");
+         status_label.setText(" Invalid user.. ");
+         loginStatus = 0;
       }
    }
 }
